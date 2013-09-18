d75da78da69eabaf927418546b0d6c84f9893eec
Therin Irwin <therin.irwin@gmail.com>
2013-09-17 18:37:40 -0700

    First revision of tip calculator app

---
+++ TipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  TipCalculator
+//
+//  Created by Therin Irwin on 9/17/13.
+//  Copyright (c) 2013 Therin Irwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ TipCalculator/TipViewController.m
@@ -0,0 +1,65 @@
+//
+//  TipViewController.m
+//  TipCalculator
+//
+//  Created by Therin Irwin on 9/17/13.
+//  Copyright (c) 2013 Therin Irwin. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmount;
+@property (weak, nonatomic) IBOutlet UILabel *total;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercent;
+
+- (IBAction)onTap:(id)sender;
+- (void)updateTip;
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.title = @"Tip Calculator";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self updateTip];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateTip];
+}
+
+- (void)updateTip {
+    float bill = _billAmount.text.floatValue;
+    
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    
+    float tip = [tipValues[self.tipPercent.selectedSegmentIndex] floatValue] * bill;
+    float totalVal = tip + bill;
+    
+    _tipAmount.text = [NSString stringWithFormat:@"$%0.2f", tip];
+    _total.text = [NSString stringWithFormat:@"$%0.2f", totalVal];
+
+}
+
+@end
